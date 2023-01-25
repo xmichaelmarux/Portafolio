dcf3d19abce50e40427561094ac83eb8baa410a3
xmichaelmarux <97730459+xmichaelmarux@users.noreply.github.com>
2023-01-25 06:18:30 +0100

    Añadiendo Proyectos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,48 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos= proyectos };
+            return View(modelo);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+
+                Titulo = "Amazon",
+                Descripcion = "ejemplo1",
+                Link = "https://amazon.com",
+                ImagenURL = "/img/lobo the witcher.jpg"
+                },
+                new Proyecto
+                {
+
+                Titulo = "Amazon",
+                Descripcion = "ejemplo2",
+                Link = "https://amazon.com",
+                ImagenURL = "/img/lobo the witcher.jpg"
+                },
+                new Proyecto
+                {
+
+                Titulo = "Amazon",
+                Descripcion = "ejemplo3",
+                Link = "https://amazon.com",
+                ImagenURL = "/img/lobo the witcher.jpg"
+                },
+                new Proyecto
+                {
+
+                Titulo = "Amazon",
+                Descripcion = "ejemplo4",
+                Link = "https://amazon.com",
+                ImagenURL = "/img/lobo the witcher.jpg"
+                }
+            };
         }
 
         public IActionResult Privacy()
